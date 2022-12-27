fe1500588bf2f0868ddf1745d285f53c971698c5
mohammed amine elazzab <ma.elazzab@gmail.com>
2022-12-27 16:27:23 +0100

    update index

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const colors = require('colors')
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
     schema,
@@ -13,5 +18,5 @@ app.use('/graphql', graphqlHTTP({
 
 
 app.listen(port, () => 
-console.log(`☕️ Server is running on port ➔  ${port} 💣`))
+console.log(`☕️ Server is running on port ➔  ${port} 💣`.green.underline.bold))
 
